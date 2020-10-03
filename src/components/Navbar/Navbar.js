f5c02719df2557ec975994d125d3be69c89c886f
syket-git <syketb@gmail.com>
2020-10-03 17:46:00 +0600

    profile structre runnig...

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,15 +8,15 @@ const NavigationBar = () => {
     <div>
       <Navbar bg="light" expand="lg">
   <Navbar.Brand href="/">
-      <img class="logo" src={logo} alt=""/>
+      <img className="logo" src={logo} alt=""/>
   </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link class="nav-link" href="/registration">Registration</Nav.Link>
-      <Nav.Link class="nav-link" href="/login">Login</Nav.Link>
-      <Nav.Link class="nav-link" href="/verify-email">Verify Email</Nav.Link>
-      <Nav.Link class="nav-link" href="/profile">Profile</Nav.Link>
+      <Nav.Link className="nav-link" href="/registration">Registration</Nav.Link>
+      <Nav.Link className="nav-link" href="/login">Login</Nav.Link>
+      <Nav.Link className="nav-link" href="/verify-email">Verify Email</Nav.Link>
+      <Nav.Link className="nav-link" href="/profile">Profile</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import './Profile.css';
+import ProfileImage from '../../img/profile.jpg';
+import EditIcon from '@material-ui/icons/Edit';
 
 const Profile = () => {
-    return (
-        <div>
-            <h1>Profile</h1>
+  return (
+    <div className="profile">
+      <div className="container-fluid">
+        <div className="row my-5">
+          <div className="col-md-8">
+            <h3 className="profile__name">Welcome Ganesh!</h3>
+
+            <div className="profile__image--box">
+              <img src={ProfileImage} alt="" className="profile__image" />
+              <div className="edit-box">
+                <label htmlFor="image">
+                  <EditIcon className="image-edit" />
+                </label>
+                <input
+                  className="file-input"
+                  type="file"
+                  name="image"
+                  id="image"
+                />
+              </div>
+            </div>
+
+            <div className="form-box">
+              <form className="profile-form">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="firstName">First Name</label>
+                      <input type="text" name="firstName" id="firstName" />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label htmlFor="lastName">Last Name</label>
+                      <input type="text" name="lastName" id="lastName" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-md-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+            dolores quo reiciendis praesentium voluptas in, eum laborum odit
+            veritatis cum id beatae temporibus reprehenderit! Necessitatibus
+            esse quas debitis eligendi atque.
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
